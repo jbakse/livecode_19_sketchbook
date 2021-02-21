adf05b1a5f1008f879b516ef4ab93ebd75a7c20a
Justin Bakse <justin@tsfim.com>
2021-02-20 20:11:21 -0600

    sketching

--- sketches/sketching/06_pixels/meta_01.js
+++ sketches/sketching/06_pixels/meta_01.js
@@ -29,7 +29,7 @@ function smin(a, b, k = 10) {
 }
 
 function draw() {
-  g.background("gray");
+  // g.background("gray");
 
   for (const a of circles) {
     a.x = noise(a.id, frameCount * 0.01, 0) * 100;
@@ -37,32 +37,35 @@ function draw() {
   }
 
   g.loadPixels();
-  for (let y = 0; y < 128; y++) {
-    for (let x = 0; x < 128; x++) {
-      // const xx = x + noise(x * 0.5, y * 0.5) * 3;
-      // const yy = y + noise(x * 0.5, y * 0.5) * 3;
-      let d = 100;
-      for (const a of circles) {
-        const c = dist(a.x, a.y, x, y) - a.r;
+  for (let i = 0; i < 128 * 32; i++) {
+    const x = floor(random(64) + random(64));
+    const y = floor(random(64) + random(64));
+    // for (let y = 0; y < 128; y++) {
+    // for (let x = 0; x < 128; x++) {
+    // const xx = x + noise(x * 0.5, y * 0.5) * 3;
+    // const yy = y + noise(x * 0.5, y * 0.5) * 3;
+    let d = 100;
+    for (const a of circles) {
+      const c = dist(a.x, a.y, x, y) - a.r;
 
-        d = smin(d, c, 5);
-      }
-
-      let i = (y * 128 + x) * 4;
-
-      // const static = noise(x, y + frameCount);
-      const static = random();
-      // const static = 1;
-      // const static = (x + y) % 2;
-
-      const ripple = sin(d + frameCount * 0.1) * 0.5 + 0.5;
-      g.pixels[i + 0] = map(d, -0, 10, ripple, 0, true) * 255;
-      g.pixels[i + 1] = map(d, -10, 10, ripple, 0, true) * 255;
-      g.pixels[i + 2] = map(d, -5, 10, ripple + static, 0, true) * 255;
-      g.pixels[i + 3] = 255;
+      d = smin(d, c, 5);
     }
-  }
 
+    let i = (y * 128 + x) * 4;
+
+    // const static = noise(x, y + frameCount);
+    const static = random();
+    // const static = 1;
+    // const static = (x + y) % 2;
+
+    const ripple = sin(d + frameCount * 0.1) * 0.5 + 0.5;
+    g.pixels[i + 0] = map(d, -0, 10, ripple, 0, true) * 255;
+    g.pixels[i + 1] = map(d, -10, 10, ripple, 0, true) * 255;
+    g.pixels[i + 2] = map(d, -5, 10, ripple + static, 0, true) * 255;
+    g.pixels[i + 3] = 255;
+    // }
+    // }
+  }
   g.updatePixels();
 
   noSmooth();

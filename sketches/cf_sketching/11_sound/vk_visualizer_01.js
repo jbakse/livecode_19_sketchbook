8ceeb0091653bd70f1b1468a955811bd01bb0195
Justin Bakse <justin@tsfim.com>
2021-04-23 16:12:41 -0500

    improve visualizer

--- sketches/cf_sketching/11_sound/vk_visualizer_01.js
+++ sketches/cf_sketching/11_sound/vk_visualizer_01.js
@@ -1,59 +1,49 @@
 // require https://cdn.jsdelivr.net/npm/tweakpane@1.5.8/dist/tweakpane.min.js
 
-// require https://cdn.jsdelivr.net/npm/p5@latest/lib/p5.min.js
+// require https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.1/p5.js
 // require https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.3.1/addons/p5.sound.js
 
 /* exported setup draw preload */
-/* globals loadSound, sampleRate */
+/* globals loadSound */
 /* globals Tweakpane */
 /* globals sketch_directory */
 
-// todo
-// trebble boost param?
-// how to capture input for this?
-// color well controlls
-// gutter size
-// level stroke size
-// range box: none, outline, solid, just bottom and top
-// range box: stroke size
-// draw arrows at low and high piano key
-// add preset reset
+// const flags = {};
+// let searchParams = new URLSearchParams(document.location.search.substring(1));
+// flags.showConfig = searchParams.get("showConfig");
 
-const params = {
-  barCount: 128,
-  fftBuckets: 2048,
-  smoothing: 0.8,
-  backColor: "#000000ff",
-  lineColor: "#ffffffff",
-  boxColor: "#ffffff99",
-  shrink: 5,
+const defaultParams = {
+  gain: 1.0,
   highBoost: 0.0,
+  barCount: 128,
+  bucketCount: 2048,
+  smoothing: 0.8,
+  boxShrink: 5,
+  barGutter: 2,
+  lineWeight: 1,
+  lineColor: "#ffffffff",
+  boxLineColor: "#ffffff99",
+  boxFillColor: "#ffffff00",
+  backColor: "#000000ff",
+  frameRate: "",
 };
 
-const baseParams = Object.assign({}, params);
+const params = Object.assign({}, defaultParams);
 
 const pane = new Tweakpane();
-pane.addButton({ title: "Play" }).on("click", () => {
-  song.play();
-});
-pane.addSeparator();
-pane.addInput(params, "barCount", { min: 0, max: params.fftBuckets });
-pane.addInput(params, "smoothing", { min: 0, max: 1 });
-pane.addInput(params, "shrink", { min: 0, max: 10 });
-pane.addInput(params, "highBoost", { min: 0, max: 1 });
-pane.addInput(params, "backColor");
-pane.addInput(params, "lineColor");
-pane.addInput(params, "boxColor");
 
-pane.addSeparator();
-pane.addButton({ title: "Save" }).on("click", saveParams);
-pane.addButton({ title: "Reset" }).on("click", resetParams);
-// pane.on("change", saveParams);
+let testSong; // p5.SoundFile
+let fft; // p5.FFT
+let audioInput; // p5.AudioIn
+
+const buckets = []; // array of bucket data
+
+//////////////////////////////////////////////////
+// Commands
 
 // import presets from local storage
 function loadParams() {
   const p = localStorage.getItem("params");
-  console.log("import", { p });
   if (p) {
     console.log("Importaing Presets");
     pane.importPreset(JSON.parse(p));
@@ -67,95 +57,179 @@ function saveParams() {
   localStorage.setItem("params", JSON.stringify(p));
 }
 
+// reset params to hardcoded defaults
 function resetParams() {
-  Object.assign(params, baseParams);
+  Object.assign(params, defaultParams);
 }
 
-let song;
-let fft;
-const buckets = [];
+// connect fft analyzer to test song
+function useSong() {
+  if (!testSong.isPlaying()) {
+    testSong.play();
+  }
+  testSong.connect();
+  audioInput.disconnect();
+  fft.setInput(testSong);
+}
+
+// connect fft analyzer to system audio input
+function useInput() {
+  testSong.stop();
+  testSong.disconnect();
+  fft.setInput(audioInput);
+}
+
+//////////////////////////////////////////////////
+// Preload
 
 function preload() {
-  console.log(loadSound);
-  song = loadSound(sketch_directory + "music/mozart_01_30.mp3");
-  // song = loadSound(sketch_directory + "music/white_noise_01.mp3");
+  testSong = loadSound(sketch_directory + "music/mozart_01_30.mp3");
+}
+
+//////////////////////////////////////////////////
+// Setup
+
+function configurePane() {
+  pane.addButton({ title: "Use Test Song" }).on("click", useSong);
+  pane.addButton({ title: "Use Input" }).on("click", useInput);
+
+  pane.addSeparator();
+
+  pane.addMonitor(params, "frameRate");
+
+  pane.addSeparator();
+
+  pane.addInput(params, "gain", { min: 0, max: 2 });
+  pane.addInput(params, "highBoost", { min: 0, max: 1 });
+  pane.addInput(params, "smoothing", { min: 0, max: 1 });
+
+  pane.addSeparator();
+
+  pane.addInput(params, "barCount", { min: 0, max: params.bucketCount });
+  pane.addInput(params, "barGutter", { min: 0, max: 10 });
+  pane.addInput(params, "boxShrink", { min: 0, max: 10 });
+
+  pane.addSeparator();
+
+  pane.addInput(params, "lineWeight", { min: 0, max: 100 });
+  pane.addInput(params, "lineColor");
+  pane.addInput(params, "boxLineColor");
+  pane.addInput(params, "boxFillColor");
+  pane.addInput(params, "backColor");
+
+  pane.addSeparator();
+
+  pane.addButton({ title: "Save" }).on("click", saveParams);
+  pane.addButton({ title: "Reset" }).on("click", resetParams);
+
+  // pane.hidden = true;
 }
 
 function setup() {
+  configurePane();
   createCanvas(1920 * 0.5, 1080 * 0.5);
 
-  song.play();
-  fft = new p5.FFT(0, params.fftBuckets);
+  // connect to system audio, configure input via browser
+  audioInput = new p5.AudioIn();
+  audioInput.start();
 
-  times(params.fftBuckets, (i) => {
+  // create fft analyzer
+  fft = new p5.FFT(0, params.bucketCount);
+
+  // initialize bucket data
+  times(params.bucketCount, (i) => {
     buckets.push({
-      i,
-      energy: 0,
-      max: 0,
-      min: 0,
-      smooth: 0,
+      i, // index
+      energy: 0, // current energy w/o smoothing
+      smooth: 0, // current energy w/ smoothing
+      max: 0, // recent max smooth (decays w/ boxShrink)
+      min: 0, // recent min smooth (decays w/ boxShrink)
     });
   });
-  // console.log(`sampleRate: ${sampleRate()}`);
-  // for (let i = 0; i < params.fftBuckets; i++) {
-  //   console.log(`${i} => ${indexToFreq(i, sampleRate(), params.fftBuckets)}`);
-  // }
+
+  // config p5 state
+  rectMode(CORNERS);
+
+  // start
+  useInput();
 }
 
 function draw() {
+  params.frameRate = Math.floor(frameRate());
+  // analyze current audio
   const spectrum = fft.analyze();
+
+  // update our bucket data
   for (const bucket of buckets) {
+    // apply adjustments
     const boost = bucket.i * params.highBoost * 0.01;
-    bucket.energy = spectrum[bucket.i] * (1 + boost);
+    bucket.energy = spectrum[bucket.i] * (1 + boost) * params.gain;
+
+    // calculate smoothed value
     bucket.smooth = lerp(bucket.smooth, bucket.energy, 1 - params.smoothing);
-    bucket.min += params.shrink;
-    bucket.max -= params.shrink;
+
+    // keep track of max and min with decay
+    bucket.min += params.boxShrink;
+    bucket.max -= params.boxShrink;
     bucket.min = min(bucket.min, bucket.smooth);
     bucket.max = max(bucket.max, bucket.smooth);
   }
 
+  // draw
   background(params.backColor);
 
   for (let i = 0; i < params.barCount; i++) {
-    const x = map(i, 0, params.barCount, 0, width);
-    const w = width / params.barCount;
-    const y = height - 10;
+    // calculate track position
+    const track_x = map(i, 0, params.barCount, 0, width);
+    const track_w = Math.max(1, width / params.barCount - params.barGutter);
+    const track_y = height - 10;
 
-    const smooth = map(buckets[i].smooth, 0, 255, 0, y);
-    const min = map(buckets[i].min, 0, 255, 0, y);
-    const max = map(buckets[i].max, 0, 255, 0, y);
-    // h *= i * 0.1;
-    // rect(x, y, w - 1, -h);
+    // calculate where the line, box top, and box bottom go
+    const smooth = map(buckets[i].smooth, 0, 255, 0, track_y);
+    const min = map(buckets[i].min, 0, 255, 0, track_y);
+    const max = map(buckets[i].max, 0, 255, 0, track_y);
 
     push();
     {
-      rectMode(CORNERS);
-      noFill();
-      stroke(params.boxColor);
-      rect(x, y - min, x + w - 2, y - max);
+      // draw box
+      fill(params.boxFillColor);
+      stroke(params.boxLineColor);
+      rect(track_x, track_y - min, track_x + track_w, track_y - max);
+
+      // draw line
       stroke(params.lineColor);
-      line(x, y - smooth, x + w - 2, y - smooth);
+      strokeWeight(params.lineWeight);
+      strokeCap(SQUARE);
+      line(track_x, track_y - smooth, track_x + track_w, track_y - smooth);
     }
     pop();
   }
 }
 
+function keyPressed() {
+  pane.hidden = !pane.hidden;
+}
+
 // frequency to index
 // https://github.com/processing/p5.js-sound/blob/8fe9ab7afd4d14cf31821015683539fb1a64161b/src/fft.js
 
-function freqToIndex(freq, sampleRate, bucketCount) {
-  const nyquist = sampleRate / 2;
-  return (freq / nyquist) * bucketCount;
-}
+// function freqToIndex(freq, sampleRate, bucketCount) {
+//   const nyquist = sampleRate / 2;
+//   return (freq / nyquist) * bucketCount;
+// }
 
-function indexToFreq(index, sampleRate, bucketCount) {
-  const nyquist = sampleRate / 2;
-  return (index / bucketCount) * nyquist;
-}
+// function indexToFreq(index, sampleRate, bucketCount) {
+//   const nyquist = sampleRate / 2;
+//   return (index / bucketCount) * nyquist;
+// }
 
 // piano frequencies range from 27.5 A0 to 4186.01 C8
 // at 44100, were looking at the first 48/256 ~20 of the spectrum for musical note base tones
 
+/**
+ * times
+ * run the function `f` `t` times, return results as array
+ */
 function times(t, f) {
   let a = [];
   for (let i = 0; i < t; i++) {
@@ -163,3 +237,14 @@ function times(t, f) {
   }
   return a;
 }
+
+// Notes:
+// Feature Ideas
+// [x] trebble boost param
+// [x] color well controls
+// [x] gutter size
+// [x] level stroke size
+// [x] add preset reset
+// [] draw arrows at low and high piano key
+// [x] show/hide options
+// [] scale canvas for full screen
